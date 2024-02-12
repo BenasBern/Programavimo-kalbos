c018ad4982734269f19a7248dc38f72b7b54297e
BenasBern <159789357+BenasBern@users.noreply.github.com>
2024-02-12 17:46:09 +0200

    Update kalkuliatorius.py
    Meniu spausdinimas vartotojui, kad pasirinktu koi veiksma nori atlikti.
    Jei pasirinkimas yra tinkamas, prasoma irasyti 2 skaicius, su kuriais norima atlikti veiksmus, jei netinkama ivesti, ismetama, kad bloga ivestis ir prasoma bandyti dar karta

--- kalkuliatorius.py
+++ kalkuliatorius.py
@@ -13,3 +13,23 @@ def daugyba(x, y):
 # Funkcija padalina
 def dalyba(x, y):
     return x / y
+
+print("Pasirinkimai: ")
+print("Sudetis: ")
+print("Atimtis: ")
+print("Daugyba: ")
+print("Dalyba: ")
+
+while True:
+    # Irasas is vartotojo
+    pasirinkimas = input("Pasirinkimas (1/2/3/4): ")
+
+    # Patikrinimas ar tai yra vienas is pasirinkimu
+    if pasirinkimas in ('1', '2', '3', '4'):
+        try:
+            pirmas = int(intput("Pirmas numeris:"))
+            antras = int(intput("Antras numeris:"))
+        except ValueError:
+            print("Netinkamas. Prasom irasyti numeri")
+            continue
+
