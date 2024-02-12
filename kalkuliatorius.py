b29560b40d7ad1fa991a5ffa691c1f80d7c95a00
BenasBern <159789357+BenasBern@users.noreply.github.com>
2024-02-12 17:39:35 +0200

    Update kalkuliatorius.py
    Funkcijos atliekancios paprasciausius aritmetinius veiksmus

---
+++ kalkuliatorius.py
@@ -0,0 +1,15 @@
+# Funkcija sudeda
+def sudetis(x, y):
+    return x + y
+
+# Funkcija atima
+def atimtis(x, y):
+    return x - y
+
+# Funkcija padaugina
+def daugyba(x, y):
+    return x * y
+
+# Funkcija padalina
+def dalyba(x, y):
+    return x / y
