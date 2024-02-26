3fe7819195ac9c350cb8840bf4dc92c6bea224c0
BenasBern <159789357+BenasBern@users.noreply.github.com>
2024-02-26 15:56:32 +0200

    Update kalkuliatorius2.py
    Pataisiau tam tikras vietas, kad veiktu viskas tinkamai ir gerai

--- kalkuliatorius2.py
+++ kalkuliatorius2.py
@@ -29,7 +29,7 @@ def hello_world():
                         <input type="text" id="test2" name="test2" value=""><br>
                         </br></br>
                         
-                    <label for="zenklas">Ženklas</label><br>   
+                    <label for="zenklas">Ženklas (+, -, *, /)</label><br>   
                         <input type="text" id="zenklas" name="zenklas" value=""><br>
                         </br></br>
 
@@ -54,76 +54,7 @@ def skaiciavimo():
         rezultatas = dalyba(skaicius1, skaicius2)
     else:
         rezultatas = "Nepalaikomas veiksmas"
-
-    return f"Operacijos rezultatas: {rezultatas}"
+    return f"Atliktos operacijos rezultatas: {rezultatas}"
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# 1. Susiediegiame Flask
-    # pip3 install Flask
-
-# 2. Importuojame
-
-'''<form>
-  <label for="test">Pirmas skaičius:</label><br>
-  <input type="text" id="test" name="fname"><br>
-  <label for="test2">Antras skaičius:</label><br>
-  <input type="text" id="test2" name="lname">
-</form>'''
-
-
-'''# Funkcija sudeda
-def sudetis(x, y):
-    return x + y
-
-# Funkcija atima
-def atimtis(x, y):
-    return x - y
-
-# Funkcija padaugina
-def daugyba(x, y):
-    return x * y
-
-# Funkcija padalina
-def dalyba(x, y):
-    if (y==0):
-        return
-    else:
-        return x / y
-
-print("Pasirinkimai: ")
-print("Sudetis: ")
-print("Atimtis: ")
-print("Daugyba: ")
-print("Dalyba: ")
-
-while True:
-    # Irasas is vartotojo
-    pasirinkimas = input("Pasirinkimas (1/2/3/4): ")
-
-    # Patikrinimas, ar tai yra vienas is pasirinkimu
-    if pasirinkimas in ('1', '2', '3', '4'):
-        try:
-            pirmas = float(input("Pirmas numeris: "))
-            antras = float(input("Antras numeris: "))
-        except ValueError:
-            print("Netinkamas. Prasom irasyti numeri")
-            continue
-
-        if pasirinkimas == '1':
-            print(pirmas, "+", antras, "=", sudetis(pirmas, antras))
-        elif pasirinkimas == '2':
-            print(pirmas, "-", antras, "=", atimtis(pirmas, antras))
-        elif pasirinkimas == '3':
-            print(pirmas, "*", antras, "=", daugyba(pirmas, antras))
-        elif pasirinkimas == '4':
-            print(pirmas, "/", antras, "=", dalyba(pirmas, antras))
-
-        # Paklausti ar vartotojas nori kito veiksmo
-        # Sustabdyti programa, jei atsakymas - ne
-        kitasskaiciavimas = input("Ar atlikti kita skaiciavima? (taip/ne): ")
-        if kitasskaiciavimas == "ne":
-          break
-    else:
-        print("Netinkama ivestis")'''
